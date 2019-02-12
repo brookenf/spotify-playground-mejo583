9d46fdb3fdb28d106dda53e848a4291f506da10c
Glitch (brookenf-spotify-playground-mejo583) <none>
2019-02-12 15:46:54 +0000

    👸🔖 Checkpoint ./server.js:3109110/12 ./public/client.js:3109110/4

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/category-playlists', function (request, response) {
 app.get('/tracks', function(request, response) { 
   //https://api.spotify.com/v1/tracks/4uLU6hMCjMI75M1A2tKUQC
   //Get the name of the specific track from ID
-  spotifyApi.getTrack({ ids : '4uLU6hMCjMI75M1A2tKUQC' })
+  spotifyApi.getTrack('4uLU6hMCjMI75M1A2tKUQC')
     .then(function(data) {
       
       //send the data
